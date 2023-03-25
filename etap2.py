7691ca5b0c5185a4f52aaac631aba4410626add2
Kolibri <97480691+SGKolibri@users.noreply.github.com>
2023-03-25 20:56:09 -0300

    Create etap2.py

---
+++ etap2.py
@@ -0,0 +1,14 @@
+class Character:
+    def __init__(self, nome, descricao, image_link, programa, animador):
+        self.nome = nome
+        self.descricao = descricao
+        self.image_link = image_link
+        self.programa = programa
+        self.animador = animador
+
+    def __str__(self):
+        return f"Nome: {self.nome}. \nDescrição: {self.descricao}. \nImagem: {self.image_link}. \nPrograma: {self.programa}. \nAnimador: {self.animador}."
+
+#Exemplo de uso
+batman = Character("Batmadasdn", "Super-herói de Gotham City", "https://upload.wikimedia.org/wikipedia/pt/8/8d/Batman_por_Jim_Lee.jpg", "Liga da Justiça", "Warner Bros")
+print(batman)
